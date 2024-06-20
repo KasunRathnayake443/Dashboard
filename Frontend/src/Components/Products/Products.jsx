9461c66ec38aec709f4e9ed4689aee0213b50385
Kasun Rathnayake <madkasunmax@gmail.com>
2024-06-20 21:12:58 +0530

    3

--- Frontend/src/Components/Products/Products.jsx
+++ Frontend/src/Components/Products/Products.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Products.css'
 
 
 const Products = () => {
+
+  const [products,setProducts] = useState([{}])
+
+  useEffect(()=>{
+    fetch("/api").then(
+      Response => Response.json()
+    ).then(data => {
+      setProducts(data)
+    })
+  } , [])
+
   return (
     <>
     <h2>Products</h2>
